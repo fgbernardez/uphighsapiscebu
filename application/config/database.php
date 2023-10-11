a5ce573d56439aceeb45fd0b29df57578e285200
christianparages <chanixthezero@gmail.com>
2023-10-11 08:48:15 +0800

    comment out local db

--- application/config/database.php
+++ application/config/database.php
@@ -97,24 +97,24 @@ $db['default'] = array(
 );
 
 // - local
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => '0.0.0.0',
-	'username' => 'root',
-	'password' => 'cde3bgt5',
-	'database' => 'sampledb2',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => '0.0.0.0',
+// 	'username' => 'root',
+// 	'password' => 'cde3bgt5',
+// 	'database' => 'sampledb2',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
